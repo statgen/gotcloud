2e69653e51071ba28e19aebb24bb91288a94ecf4
trubetsk <trubetsk@umich.edu>
2015-08-06 13:55:32 -0400

    Added a simple snpcall test for streaming csra input from NCBI. Again, this is not yet a full test. It needs to be integrated into the testing framework that MK uses. It also uses file paths and executables specific to my repository on snowwhite.

---
+++ test/stream/remote_csra/snpcall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+/net/snowwhite/home/trubetsk/projects/gotcloud/gotcloud snpcall \
+  --region 20:36000000-37000000 \
+  --numjobs 1 \
+  --conf /net/snowwhite/home/trubetsk/projects/gotcloud/test/stream/remote_csra/snpcall.conf
+
+
