dd08e715c4e3c08b861e4bd1adebf8ba90af5daf
trubetsk <trubetsk@umich.edu>
2015-08-12 10:09:01 -0400

    made conf file path relative

--- test/stream/remote_csra/snpcall.sh
+++ test/stream/remote_csra/snpcall.sh
@@ -2,6 +2,6 @@
 /net/snowwhite/home/trubetsk/projects/gotcloud/gotcloud snpcall \
   --region 20:36000000-37000000 \
   --numjobs 1 \
-  --conf /net/snowwhite/home/trubetsk/projects/gotcloud/test/stream/remote_csra/snpcall.conf
+  --conf snpcall.conf
 
 
